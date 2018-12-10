925314c75eb61d7c076ff4f9167d898f29edf0f2
reed1989 <31931412+reed1989@users.noreply.github.com>
2018-12-10 20:42:27 +0800

    test ut test

--- script/ut_run.py
+++ script/ut_run.py
@@ -8,28 +8,38 @@ import subprocess
 def main():
     make_file_path = sys.argv[1]
     dir_path = os.path.dirname(os.path.realpath(make_file_path))
-    print("dir_path is:" + dir_path)
 
-    p_clean = subprocess.run(["make clean", "-f", make_file_path],
-                             shell=True,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
-    print(p_clean)
-    print(p_clean.stdout.decode('utf-8'))
+    p_clean = subprocess.Popen(["make clean", "-f", make_file_path],
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               cwd=dir_path)
+    p_clean.wait()
+    # for line in p_clean.stdout.readline():
+    #     print(line)
+    print(p_clean.stdout.read().decode('utf-8'))
 
-    p_make = subprocess.run(["make", "-f", make_file_path],
-                            shell=True,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
-    # p_make.wait()
-    print(p_make.stdout.decode('utf-8'))
+    p_make = subprocess.Popen(["make", "-f", make_file_path],
+                              shell=True,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT,
+                              cwd=dir_path)
+    p_make.wait()
+    # for line in p_clean.stdout.readline():
+    #     print(line)
+    print(p_make.stdout.read().decode('utf-8'))
 
     run_test_file = os.path.join(dir_path, "./run_test")
-    p_run = subprocess.run([run_test_file],
-                           shell=True,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT)
-    print(p_run.stdout.decode('utf-8'))
+    p_run = subprocess.Popen([run_test_file],
+                             shell=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT,
+                             cwd=dir_path)
+    p_run.wait()
+    # for line in p_clean.stdout.readline():
+    #     print(line)
+    print(p_run.stdout.read().decode('utf-8'))
+
 
 
 if __name__ == '__main__':
