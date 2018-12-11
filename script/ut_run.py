21da78f2461a104196b61b57a5be60e41d5b44cb
reed1989 <31931412+reed1989@users.noreply.github.com>
2018-12-11 10:28:00 +0800

    ut test python

--- script/ut_run.py
+++ script/ut_run.py
@@ -9,16 +9,21 @@ def main():
     make_file_path = sys.argv[1]
     dir_path = os.path.dirname(os.path.realpath(make_file_path))
 
-#    print("Start to make clean")
-#    p_clean = subprocess.Popen(["make clean", "-f", make_file_path],
-#                               shell=True,
-#                               stdout=subprocess.PIPE,
-#                               stderr=subprocess.STDOUT,
-#                               cwd=dir_path)
-#    p_clean.wait()
+    print("Start to make clean")
+    p_clean = subprocess.Popen(["make clean", "-f", make_file_path],
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               cwd=dir_path)
+    p_clean.wait()
     # for line in p_clean.stdout.readline():
     #     print(line)
 #    print(p_clean.stdout.read().decode('utf-8'))
+    result = p_clean.returncode
+    if result == 0:
+      print(p_clean.stdout.read().decode('utf-8'))
+    else:
+      print(p_clean.stderr.read().decode('utf-8'))
 
     print("Start to make make")
     p_make = subprocess.Popen(["make"],
@@ -29,7 +34,13 @@ def main():
     p_make.wait()
     # for line in p_clean.stdout.readline():
     #     print(line)
-    print(p_make.stdout.read().decode('utf-8'))
+    result = p_make.returncode
+    if result == 0:
+      print(p_make.stdout.read().decode('utf-8'))
+    else:
+      print(p_make.stderr.read().decode('utf-8'))
+    
+
 
     print("Start to make execute the test case")
     run_test_file = os.path.join(dir_path, "./run_test")
@@ -41,7 +52,11 @@ def main():
     p_run.wait()
     # for line in p_clean.stdout.readline():
     #     print(line)
-    print(p_run.stdout.read().decode('utf-8'))
+    result = p_run.returncode
+    if result == 0:
+      print(p_run.stdout.read().decode('utf-8'))
+    else:
+      print(p_run.stderr.read().decode('utf-8'))
 
 
 
